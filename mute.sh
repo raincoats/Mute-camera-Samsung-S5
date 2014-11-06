8484c20bf15139ea81b1a65733d8e2bad23cef0d
raincoats <raincoats@users.noreply.github.com>
2014-11-06 11:32:08 +1100

    update whitespace

--- mute.sh
+++ mute.sh
@@ -8,15 +8,15 @@ function silence_phone(){
 
 	# This is where the magic happens!
 	# Camera_empty.ogg is an empty sound.
-	for sound in "camera_click.ogg" \
-                 "Camera_click_short.ogg" \
-                 "Shutter.ogg" \
-                 "Shutter_multiple.ogg" \
-                 "Camera_Timer.ogg" \
-                 "Camera_Timer_2sec.ogg" \
-                 "lens_flare_lock.ogg" \
+	for sound in "camera_click.ogg"          \
+                 "Camera_click_short.ogg"        \
+                 "Shutter.ogg"                   \
+                 "Shutter_multiple.ogg"          \
+                 "Camera_Timer.ogg"              \
+                 "Camera_Timer_2sec.ogg"         \
+                 "lens_flare_lock.ogg"           \
                  "lens_flare_unlock_silence.ogg" \
-                 "Auto_focus.ogg" \
+                 "Auto_focus.ogg"                \
                  "Auto_focus_error.ogg";
 	do 
 		echo "mv $sound $sound.backup";
