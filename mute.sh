a9873a5406ad70dfb27232efa0f9cf098c696b51
raincoats <tomshaddock@outlook.com>
2014-11-06 12:38:23 +1100

    v2

--- mute.sh
+++ mute.sh
@@ -1,12 +1,9 @@
 function raincoats(){
 
-	# mounting the fs read/write
-	mount -o remount,rw /system 
-	cd /system/media/audio/ui
+    mount -o remount,rw /system
+    cd /system/media/audio/ui
 
-	# This is where the magic happens!
-	# Camera_empty.ogg is an empty sound.
-	for sound in "camera_click.ogg"          \
+    for sound in "camera_click.ogg"          \
                  "Camera_click_short.ogg"        \
                  "Shutter.ogg"                   \
                  "Shutter_multiple.ogg"          \
@@ -15,16 +12,16 @@ function raincoats(){
                  "lens_flare_lock.ogg"           \
                  "lens_flare_unlock_silence.ogg" \
                  "Auto_focus.ogg"                \
-                 "Auto_focus_error.ogg";
-	do 
-		mv $sound $sound.backup;
-		ln -s Camera_empty.ogg $sound;
+                 "Auto_focus_error.ogg"          \
+                 "camera_focus.ogg";
+    do
+        mv $sound $sound.backup;
+        ln -s Camera_empty.ogg $sound;
+    done
 
-	done
+    mount -o remount,ro /system || reboot
 
-	# Making the fs read-only again.
-	# You can always `reboot now` instead if you like
-	mount -o remount,ro /system
+    echo "Done. Try taking a photo now."
 }
 
 raincoats
